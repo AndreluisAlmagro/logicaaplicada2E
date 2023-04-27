8e4a1f353801da8e1acd8b2704b36e06c52be456
Andre Luis Almagro de Amorim <rm86533@estudante.fieb.edu.br>
2023-04-27 09:57:28 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    // laço enquanto - while
+int(contador<10) {
+    System.out.println("contador = " + contador);
+    // incremento
+    contador = contador + 1;
+}
+    //laço do para faça - for
+    for(
+    int x = 0;
+    x < 10;x++
+
+    {
+        System.out.println("contador = " + x);
+    }
+
+    //faça enquanto - SD while
+    int cont = 1;
+    do
+
+    {
+        //processo
+        System.out.println(cont);
+        cont++; //complemento
+        while (cont < 10) ;
+    }
+}
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
